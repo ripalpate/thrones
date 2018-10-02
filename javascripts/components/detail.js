3446ff7dc299b3e863b9c2109692e195b327038a
Ripal Patel <ripal212007@gmail.com>
2018-10-02 18:49:20 -0500

    displaying single card on click

---
+++ javascripts/components/detail.js
@@ -0,0 +1,20 @@
+import {printToDom} from "../helper/util.js";
+
+const detailsBuilder =(character)=>{
+    //console.log('character inside details', character);
+    let domString = '';
+    domString +=`<div class="col-6 offset-md-3">`
+    domString +=    `<div class="row">`;
+    domString +=        `<div class="col">`;
+    domString +=            `<img src="${character.imageUrl}" alt="${character.name}">`;
+    domString +=        `</div>`;
+    domString +=        `<div class="col">`;
+    domString +=            `<h1>${character.name}</h1>`;
+    domString +=            `<h3>${character.house}</h3>`;
+    domString +=        `</div>`;
+    domString +=    `</div>`;
+    domString +=`</div>`
+
+    printToDom(domString);
+}
+export{detailsBuilder};
