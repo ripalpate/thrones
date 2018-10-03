3e64d2620593b5d7ffc7d7373b579856cde61570
Ripal Patel <ripal212007@gmail.com>
2018-10-02 20:58:20 -0500

    added XHR

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,8 @@
 import { charactersBuilder } from "./components/characters.js";
+import {getCharacters} from './data/charactersData.js'
 
 const initializeApp = () => {
-    charactersBuilder();
+    // charactersBuilder();
+    getCharacters();
 }
 initializeApp();
